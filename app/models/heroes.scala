b900b59a2b6dd0f0bfeff78ff9825454e0ce7530
luke <nerorevenge@gmail.com>
2015-12-29 17:04:34 -0500

    Added model

---
+++ app/models/heroes.scala
@@ -0,0 +1,11 @@
+package models
+
+import slick.driver.PostgresDriver.api._
+
+class Heroes(tag: Tag) extends Table[(String, String)](tag,"hero_registry"){
+  def classRank = column[String]("class_rank", O.PrimaryKey)
+  def name = column[String]("name")
+
+  def * = (classRank, name)
+}
+
